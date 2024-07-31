a05e70a5cdf9a50d0c653349a19efc5dc878dc81
kuldeepk2020 <153076185+kuldeepk2020@users.noreply.github.com>
2024-07-31 23:49:25 +0530

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title('KULDEEP YADAV')
+st.write("ABOUT: An Enthusiastic Data Scientist with experience in working over large data sets, getting meaningful extracts also analyzing data insights and deliverables from different Departments. In performing EDA to understand the data and using BI tools for visualization. Deploying ML and advanced models, with Deep Learning as specialization. Along with experience, duly equipped with Post-Graduation in Machine Learning and Artificial Intelligence. Looking forward to combining my expertise and skills in the Data Science domain so that I can contribute to the optimal level in the growth of the company.")
+
+
+
+
+
+
+
+
+
+
+
